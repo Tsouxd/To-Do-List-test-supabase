9c9cace9ed0f4595dbdb4482f4d8aaef3fee57be
Tsou Rakotoson <tsourakotoson0@gmail.com>
2026-04-01 16:04:31 +0300

    maj du jour

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
+from dotenv import load_dotenv # Import pour charger le .env
 from supabase import create_client, Client
+
+# Charger les variables d'environnement du fichier .env
+load_dotenv()
 
 app = Flask(__name__)
 
-# Configuration Supabase
-url = "https://uwwtqcnwozpcnfswhziq.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3d3RxY253b3pwY25mc3doemlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwMzk1NjIsImV4cCI6MjA5MDYxNTU2Mn0.ycbt3azHdlsJN1i0mwv3ntyCTnc6-zPP3Dvyje7ak_U"
-supabase: Client = create_client(url, key)
+# Récupération des variables d'environnement
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+# Initialisation du client Supabase
+if not url or not key:
+    print("Erreur: Les variables d'environnement SUPABASE_URL ou SUPABASE_KEY sont manquantes.")
+else:
+    supabase: Client = create_client(url, key)
 
 # --- ROUTES ---
 
-# LIRE (Afficher l'interface)
 @app.route('/')
 def index():
     # On récupère les tâches triées par ID
@@ -19,7 +27,6 @@ def index():
     tasks = response.data
     return render_template('index.html', tasks=tasks)
 
-# CRÉER
 @app.route('/add', methods=['POST'])
 def add():
     title = request.form.get('title')
@@ -27,18 +34,17 @@ def add():
         supabase.table("tasks").insert({"title": title}).execute()
     return redirect(url_for('index'))
 
-# METTRE À JOUR (Statut terminé)
 @app.route('/update/<int:task_id>/<string:status>')
 def update(task_id, status):
     new_status = (status == 'true')
     supabase.table("tasks").update({"completed": new_status}).eq("id", task_id).execute()
     return redirect(url_for('index'))
 
-# SUPPRIMER
 @app.route('/delete/<int:task_id>')
 def delete(task_id):
     supabase.table("tasks").delete().eq("id", task_id).execute()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    # Rappel : utilise port=5001 si le port 5000 est déjà pris par ton système
     app.run(debug=True, port=5000)
